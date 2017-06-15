920fe9770f2c06cf6d46895cb4208120494da6aa
陆宁 <2982432751@qq.com>
2017-06-15 09:45:58 +0800

    no message

---
+++ VerifyProject/VerifyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VerifyProject
+//
+//  Created by 陆宁 on 2017/6/15.
+//  Copyright © 2017年 LN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VerifyProject/VerifyProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VerifyProject
+//
+//  Created by 陆宁 on 2017/6/15.
+//  Copyright © 2017年 LN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
